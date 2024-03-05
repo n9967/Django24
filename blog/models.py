2b0fcfb19dbcaf01197e287f28c76fba75404640
Dean9967 <nhellalat@ivytech.edu>
2024-03-04 21:23:39 -0500

    My django forms/security  commit final

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,6 @@ from django.db import models
 from django.utils import timezone
 
 
-
-# Create your models here.
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -16,23 +14,21 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
-    
     def __str__(self):
         return self.title
-
+    
 class Comment(models.Model):
-    post = models.ForeignKey('blog.post', on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
     author = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
 
-    def approve(self)
+    def approve(self):
         self.approved_comment = True
         self.save()
-    
+
     def __str__(self):
         return self.text
-
     
-
+    
